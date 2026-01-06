347e69f40d2dd392e25217297f07fbd94a5513e9
karanbgit <karanbarale7047@gmail.com>
2026-01-06 11:50:33 +0530

    Add initial REST API data structure for frontend components including qualities, dishes, team members, testimonials, and navigation links

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import HeroSection from "../../components/HeroSection";
 
 function Home() {
   return (
     <>
-      <h1>Home Page </h1>
+      <HeroSection />
     </>
   );
 }
