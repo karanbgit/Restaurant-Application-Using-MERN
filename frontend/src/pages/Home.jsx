e92d17258f1f6cbe1d081ac424fe26db1d28798f
karanbgit <karanbarale7047@gmail.com>
2026-01-06 12:15:12 +0530

    feat: add Navbar, About, Menu, and Qualities components with initial layout and data integration

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import HeroSection from "../../components/HeroSection";
+import Navbar from "../../components/Navbar";
+import About from "../../components/About";
+import Qualities from "../../components/Qualities";
+import Menu from "../../components/Menu";
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <About />
+      <Qualities />
+      <Menu />
     </>
   );
 }
